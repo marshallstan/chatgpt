3dfdda6cbfdc829c7618f323161fc5c92f851f1e
Marshall <narutowxl@gmail.com>
2023-03-12 18:34:47 +0800

    CustomHeader

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -1,7 +1,25 @@
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow
+} from 'react-chat-engine-advanced'
+import Header from '@/components/CustomHeader'
+
 const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    'chosen1',
+    '1111'
+  )
+
   return (
-    <div>
-      Chat
+    <div style={{ flexBasis: '100%' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: '100vh' }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
     </div>
   )
 }
